bf451c872338aa3bc7434eead11a8f0ceb117939
jesrah <jesrah@gmail.com>
2018-09-04 16:33:06 -0700

    created sagas for HomePage

---
+++ app/containers/HomePage/saga.js
@@ -0,0 +1,28 @@
+import { call, put, takeEvery } from 'redux-saga/effects';
+import { LOAD_SECRETS } from 'containers/App/constants';
+import { secretsLoaded, secretsLoadingError } from 'containers/App/actions';
+
+import request from 'utils/request';
+
+export const requestURL = `http://localhost:3000/secretkeeper`;
+
+/**
+ * Strings request/response handler
+ */
+export function* getSecrets() {
+  try {
+    // Call our request helper (see 'utils/request')
+    const secrets = yield call(request, requestURL);
+    yield put(secretsLoaded(secrets));
+  } catch (err) {
+    yield put(secretsLoadingError(error));
+  }
+}
+
+/**
+ * Root saga manages watcher lifecycle
+ */
+export default function* secretsData() {
+  // Saga for action LOAD_SECRETS, responds with getSecrets
+  yield takeEvery(LOAD_SECRETS, getSecrets);
+}
