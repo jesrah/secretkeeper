e760fadd8a16bb1b6e36552321fe4cd5d8e1ef9d
jesrah <jesrah@gmail.com>
2018-09-04 13:31:11 -0700

    Created NavBar component using history.push instead of <Link> to connect with React Router

--- app/components/NavBar/index.js
+++ app/components/NavBar/index.js
@@ -1,18 +1,34 @@
 import styled from 'styled-components';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
-const NavBar = styled.button`
-  width: 100%;
-  outline: 0;
-  border: 0;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
-  line-height: 0.2;
+// const NavBar = styled.button`
+//   width: 100%;
+//   outline: 0;
+//   border: 0;
+//   height: 100%;
+//   justify-content: center;
+//   align-items: center;
+//   line-height: 0.2;
+ 
+//   ${({ active }) => active && `
+//     background: pink;
+//   `}
+// `;
 
-  ${({ active }) => active && `
-    background: pink;
-  `}
-`;
 
+// export default NavBar;
 
-export default NavBar;
+const NavBar = React.createClass({
+  render() {
+    return (
+      <div>
+        <ul>
+          <a onClick={() => history.push('Share') }>Share</a>
+          <a onClick={() => history.push('Read') }>Read</a>
+        </ul>
+      </div>
+    )
+  }
+})
