0e0b795a55b26c6fe289f5c5ff5350eacb6ad7af
jesrah <jesrah@gmail.com>
2018-09-04 14:40:37 -0700

    Added padding to space the navbar elements li elements apart from each other for readability.

---
+++ app/components/HorizontalList/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const HorizontalList = styled.li`
+	display: inline;
+	list-style-type: none;
+	padding: 0.5em;
+	margin: 1em;
+	text-align: center;
+`;
+
+export default HorizontalList;
