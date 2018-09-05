779ca8404d47acea9d880bfb11fdc4bbbf607af2
jesrah <jesrah@gmail.com>
2018-09-05 15:47:41 -0700

    Combined sagas

---
+++ app/saga.js
@@ -0,0 +1,6 @@
+import HomePageSaga from './containers/HomePage/saga';
+import ListPageSaga from './containers/ListPage/saga';
+ 
+const sagas = [HomePageSaga, ListPageSaga];
+
+ export default sagas;
