ba949dd574c4dd36a8fa05dd990ab078dec60bca
jesrah <jesrah@gmail.com>
2018-09-05 15:49:00 -0700

    Created REST functions for post and request calls

---
+++ app/utils/post.js
@@ -0,0 +1,11 @@
+export default function post(url = ``, secret) {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ secret }),
+  })
+    .then(response => response) 
+    .catch(error => console.error(`Error occured:`, error));
+}
