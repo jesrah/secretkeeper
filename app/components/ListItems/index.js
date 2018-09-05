575024017597fdd4412d5ac556808a06ce77466e
jesrah <jesrah@gmail.com>
2018-09-05 13:58:56 -0700

    Added styling to make list items and bullets appear centered

--- app/components/ListItems/index.js
+++ app/components/ListItems/index.js
@@ -2,6 +2,8 @@
 import styled from 'styled-components';
 
 const ListItems = styled.li`
+	list-style-type: circle;
+	text-align: center;
 `;
 
 export default ListItems;
--- app/components/Ul/index.js
+++ app/components/Ul/index.js
@@ -4,11 +4,10 @@ import styled from 'styled-components';
 const Ul = styled.ul`
 	text-align: center;
 	background-color: papayawhip;
-	color: black;
+	list-style-position: inside;
+	color: darkslategray;
 	padding: 4em;
 	margin: 0;
-
-
 `;
 
 export default Ul;
